cec671cb9b2a5d7591ca97f1e00e77c2641eb9e0
Datta <2096688@cognizant.com>
2022-04-23 21:47:56 +0530

    example of standard req,res using express

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,12 +1,14 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Post, Put, Delete, Body } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Req, Res } from '@nestjs/common';
 import { CreateItemDto } from './dto/create-item.dto';
+import { Request, Response } from 'express';
 
 @Controller('items')
 export class ItemsController {
     @Get()
-    findall(){
-        return 'all items'
+    findall(@Req() req: Request, @Res() res: Response): Response{
+        console.log(req.url)
+        return res.send(req.url)
     }
     @Post()
     create(@Body() createItem: CreateItemDto){
