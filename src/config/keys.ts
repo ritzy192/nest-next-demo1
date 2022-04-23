5d37917250f5f35d5f277e5c8a1046c59371a2f2
Datta <2096688@cognizant.com>
2022-04-24 01:14:34 +0530

    installed mongoose and connected to atlas database

---
+++ src/config/keys.ts
@@ -0,0 +1,4 @@
+/* eslint-disable prettier/prettier */
+export default {
+    mongoURI: 'mongodb+srv://ritzy192:Osbth4whZaiaR4jk@cluster0.7xxc4.mongodb.net/NestSampledb?retryWrites=true&w=majority'
+}
